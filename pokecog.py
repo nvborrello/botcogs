cb1a7872eed6bfbe49a753d0eec799555cd9049e
Nick Borrello <92342882+nborrello@users.noreply.github.com>
2022-05-21 05:43:09 -0400

    fix2

--- pokecog.py
+++ pokecog.py
@@ -8,7 +8,7 @@ class PokeCog(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def startsearch(self, ctx, pokemon: str):
+    async def startsearch(self, ctx):
         # global spam_loop
 
         # @tasks.loop(seconds=1)
@@ -16,7 +16,7 @@ class PokeCog(commands.Cog):
         #     await ctx.send(q)
 
         # spam_loop.start(pokemon)
-        await ctx.send("Now spamming " + pokemon)
+        await ctx.send("Now spamming ")
 
     # @commands.command()
     # async def stopsearch(self, ctx: commands.Context):
