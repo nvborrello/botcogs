601d5c873a3df0825b01592ee85437a8c0f0a8e9
Nick Borrello <92342882+nborrello@users.noreply.github.com>
2022-05-23 06:16:45 -0400

    sdfsdf

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -47,9 +47,8 @@ class BlackJack(commands.Cog):
                 await ctx.send(f'Your Cards:\n{playerCards}\n')
                 time.sleep(1)
                 await ctx.send("\nWould you like to draw another card? (y/n)")
-                msg = await self.wait_for('message', check=check, timeout=10)
-
-                if msg.content == 'y' or 'Y':
+                msg = await self.wait_for("message", check=lambda m:m.author==ctx.author and m.channel.id==ctx.channel.id)
+                if msg.content.lower in ("y", "yes"):
                     gameMode == 'Continue'
                 else:
                     gameMode == 'Flip'
