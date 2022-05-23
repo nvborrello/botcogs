f77a77c7b3b64059e2266e332e32f3f1ca909c36
Nick Borrello <92342882+nborrello@users.noreply.github.com>
2022-05-23 04:55:41 -0400

    sfakjsfaf;j

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -8,6 +8,8 @@ class Card:
     def __init__(self, suit, val):
         self.suit = suit
         self.value = val
+    def toString(self):
+        return (f'{self.value} of {self.suit}')
 
 for s in ["Spades", "Clubs", "Diamonds", "Hearts"]:
     for v in range(1, 14):
@@ -24,6 +26,10 @@ class BlackJack(commands.Cog):
         user = ctx.author
         await ctx.send(f'Starting a game of Blackjack...\n {user} has bet ${bet}')
         drawn = random.sample(deck, 2)
+        str1 = drawn[0].toString()
+        str2 = drawn[1].toString()
+        await ctx.send(str1)
+        await ctx.send(str2)
         await ctx.send(f'Card 1: {drawn[0].value} of {drawn[0].suit}\nCard 2: {drawn[1].value} of {drawn[1].suit}')
 
        
