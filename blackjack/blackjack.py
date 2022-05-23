499b6551be8e533fc66b4768643e8035283a6fa9
Nick Borrello <92342882+nborrello@users.noreply.github.com>
2022-05-23 04:30:12 -0400

    sdfaf

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,3 +1,4 @@
+import math
 from redbot.core import commands
 from discord.ext import tasks
 import random
@@ -8,6 +9,24 @@ class Card:
     def __init__(self, suit, val):
         self.suit = suit
         self.value = val
+
+    def print(self):
+        curVal = int(self.value)
+        newValue = ''
+        if curVal > 10:
+            match (curVal-10):
+                case 1:
+                    newValue = 'King'
+                case 2:
+                    newValue = 'Queen'
+                case 3:
+                    newValue = 'Ace'
+                case 4:
+                    newValue = 'Jack'
+        else:
+            newValue = self.value
+        printer = f'{newValue} of {self.suit}'
+        return printer
 
 for s in ["Spades", "Clubs", "Diamonds", "Hearts"]:
     for v in range(1, 14):
@@ -25,6 +44,6 @@ class BlackJack(commands.Cog):
         user = ctx.author
         await ctx.send(f'Starting a game of Blackjack...\n {user} has bet ${bet}')
         drawn = random.sample(deck, 2)
-        await ctx.send(f'Card 1: {drawn[0].value} of {drawn[0].suit}\n Card 2: {drawn[1].value} of {drawn[1].suit}')
+        await ctx.send(f'Card 1: {drawn[0].print}\nCard 2: {drawn[1].print}')
 
        
