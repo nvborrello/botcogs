b0dfd0b72b948fe86dc8e48d3289b751bc4e309d
Nick Borrello <92342882+nborrello@users.noreply.github.com>
2022-05-23 04:19:27 -0400

    asfl;akfaskfj

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -11,7 +11,20 @@ class Card:
 
 for s in ["Spades", "Clubs", "Diamonds", "Hearts"]:
     for v in range(1, 14):
-        deck.append(Card(s, v))
+        stringy = ''
+        if v > 10:
+            match (v-10):
+                case 1:
+                    stringy = 'Ace'
+                case 2:
+                    stringy = 'King'
+                case 3:
+                    stringy = 'Queen'
+                case 4:
+                    stringy = 'Jack'
+        else:
+            stringy = v
+        deck.append(Card(s, stringy))
 
 
 class BlackJack(commands.Cog):
@@ -25,6 +38,6 @@ class BlackJack(commands.Cog):
         user = ctx.author
         await ctx.send(f'Starting a game of Blackjack...\n {user} has bet ${bet}')
         drawn = random.sample(deck, 2)
-        await ctx.send(f'Card 1: {drawn[0].value} of {drawn[0].suit}')
+        await ctx.send(f'Card 1: {drawn[0].value} of {drawn[0].suit}\n Card 2: {drawn[1].value} of {drawn[1].suit}')
 
        
