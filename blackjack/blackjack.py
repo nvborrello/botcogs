b5e811e87e1732df08324d4f8491adce7bbec508
Nick Borrello <92342882+nborrello@users.noreply.github.com>
2022-05-23 04:11:05 -0400

    adsfasdf

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -25,6 +25,6 @@ class BlackJack(commands.Cog):
         user = ctx.author
         await ctx.send(f'Starting a game of Blackjack...\n {user} has bet ${bet}')
         drawn = random.sample(deck, 2)
-        await ctx.send(f'Card 1: {drawn[0].val} of {drawn[0].suit}')
+        await ctx.send(f'Card 1: {drawn[0].value} of {drawn[0].suit}')
 
        
